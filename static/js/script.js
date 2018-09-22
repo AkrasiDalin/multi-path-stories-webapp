a9ef48d311f9f1cb3deec7cc8e9f77152abca1c4
AkrasiDalin <dalin.jo@yahoo.com>
2018-09-22 18:36:50 +0100

    Add transition to moon. Fix clouds erro which was reseting all positions at the same time

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,9 @@
 (function(){
 
+  var moon = document.querySelector(".moon");
   var clouds = document.querySelectorAll(".cloud");
+
+  moon.style.transform = `translate3d(0px, 0px, 0px)`;
 
   for(let i=0; i < clouds.length; ++i){
     setLeft(clouds[i], i);
@@ -11,26 +14,30 @@
     item.style.transform = `translate3d(${i * 100}px, 0px, 0px)`;
   }
 
-  function increment(item){
+  function increment(item,i){
     let xPosition = +item.style.transform.split('px')[0].split('(')[1];
-    let newXPosition = reset(xPosition);
+    let newXPosition = reset(xPosition,i);
     
     item.style.transform = `translate3d(${newXPosition}px, 0px, 0px)`;
   }
 
-  function reset(xPosition){
+  function reset(xPosition, i){
     if(xPosition >= window.innerWidth){
       return 0;
     }
     else {
-      return xPosition + 1;
+      return xPosition + i;
     }
   }
 
 
   setInterval(function(){
-    clouds.forEach(increment);
-  },50)
+    clouds.forEach(increment,1);
+  },100)
+
+  setInterval(function(){
+    increment(moon,1);
+  },150)
 
 
 })()
